4c6464c5934bda9913210f45278656f94bbce693
Enrico Buonanno <enrico.buonanno@gmail.com>
2017-08-15 21:29:42 +1000

    added TaskValidation class and tests

---
+++ LaYumba.Functional.Tests/MonadStacks/TaskValidation.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+using System.Threading.Tasks;
+
+namespace LaYumba.Functional.Tests
+{
+   public class TaskValidationTest
+   {
+      [Fact]
+      public void SingleSelectClauseCompiles()
+      {
+         Task<int> t1 = 
+            from i in Async(1)
+            select i;
+
+         Validation<int> v1 =
+            from i in Valid(1)
+            select i;
+
+         Task<Validation<int>> tv1 =
+            from i in Async(Valid(1))
+            select i;
+      }
+
+      [Fact]
+      public void TwoSelectClauseCompiles()
+      {
+         Task<int> t1 = 
+            from i in Async(1)
+            from j in Async(2)
+            select i + j;
+
+         Validation<int> v1 =
+            from i in Valid(1)
+            from j in Valid(2)
+            select i + j;
+
+         Task<Validation<int>> tv1 =
+            from i in Async(Valid(1))
+            from j in Async(Valid(2))
+            select i + j;
+      }
+
+      [Fact]
+      public void ThreeSelectClauseCompiles()
+      {
+         Task<int> t1 = 
+            from i in Async(1)
+            from j in Async(2)
+            from k in Async(3)
+            select i + j + k;
+
+         Validation<int> v1 =
+            from i in Valid(1)
+            from j in Valid(2)
+            from k in Valid(3)
+            select i + j + k;
+
+         Task<Validation<int>> tv1 =
+            from i in Async(Valid(1)).Stack()
+            from j in Async(Valid(2))
+            from k in Async(Valid(3))
+            select i + j + k;
+      }
+
+      [Fact]
+      public void FourSelectClauseCompiles()
+      {
+         Task<int> t1 = 
+            from i in Async(1)
+            from j in Async(2)
+            from k in Async(3)
+            from m in Async(4)
+            select i + j + k + m;
+
+         Validation<int> v1 =
+            from i in Valid(1)
+            from j in Valid(2)
+            from k in Valid(3)
+            from m in Valid(4)
+            select i + j + k + m;
+
+         Task<Validation<int>> tv1 =
+            from i in Async(Valid(1)).Stack()
+            from j in Async(Valid(2))
+            from k in Async(Valid(3))
+            from m in Async(Valid(4))
+            select i + j + k + m;
+      }
+   }
+}
--- LaYumba.Functional/MonadStacks/TaskValidationMonad.cs
+++ LaYumba.Functional/MonadStacks/TaskValidationMonad.cs
@@ -5,32 +5,31 @@ namespace LaYumba.Functional
 {
    using static F;
 
+   public class TaskValidation<T>
+   {
+      public Task<Validation<T>> Value { get; }
+
+      public TaskValidation(Task<Validation<T>> value) 
+         => Value = value;
+
+      public static implicit operator Task<Validation<T>>(TaskValidation<T> tv) 
+         => tv.Value;
+   }
+
    public static class TaskValidationMonad
    {
-      //public static Validation<Task<V>> SelectMany<T, U, V>
-      //   (this Validation<T> self
-      //   , Func<T, Task<U>> bind
-      //   , Func<T, U, V> project)
-      //   => self.Map(t => bind(t).Map(u => project(t, u)));
-
-      //public static Task<Validation<RR>> SelectMany<T, R, RR>
-      //   (this Validation<T> val              // Validation<T>
-      //   , Func<T, Task<Validation<R>>> bind  // -> (T -> Task<Validation<R>>)
-      //   , Func<T, R, RR> project)
-      //   => val.TraverseBind(t => bind(t).Map(vr => vr.Map(r => project(t, r))));
-
+      public static TaskValidation<T> Stack<T>(this Task<Validation<T>> @this)
+         => new TaskValidation<T>(@this);
 
       public static Task<Validation<U>> Select<T, U>
          (this Task<Validation<T>> self
          , Func<T, U> mapper)
          => self.Map(x => x.Map(mapper));
 
-      // Task<Validation<T>> `bind` T -> Task<Validation<R>>
-
-      public static Task<Validation<R>> SelectMany<T, R>
-         (this Task<Validation<T>> task       // Task<Validation<T>> 
-         , Func<T, Task<Validation<R>>> bind) // -> (T -> Task<Validation<R>>)
-         => task.Bind(vt => vt.TraverseBind(bind));
+      // public static Task<Validation<R>> SelectMany<T, R>
+      //    (this Task<Validation<T>> task       // Task<Validation<T>> 
+      //    , Func<T, Task<Validation<R>>> bind) // -> (T -> Task<Validation<R>>)
+      //    => task.Bind(vt => vt.TraverseBind(bind));
 
       public static Task<Validation<RR>> SelectMany<T, R, RR>
          (this Task<Validation<T>> task       // Task<Validation<T>> 
@@ -49,48 +48,15 @@ namespace LaYumba.Functional
       //      return valR.Map(r => project(t, r));
       //   });
       //}
-   
-      
-      
-      
-      // Task<Validation<T>> `bind` T -> Validation<R>
 
-      //public static Task<Validation<RR>> SelectMany<T, R, RR>
-      //   (this Task<Validation<T>> task    // Task<Validation<T>> 
-      //   , Func<T, Validation<R>> bind     // -> (T -> Validation<R>)
-      //   , Func<T, R, RR> project)
-      //   => task.Map(vt => vt.Bind(t => bind(t).Map(r => project(t, r))));
-
-      // Task<Validation<T>> `bind` T -> Task<R>
-
-      //public static Task<Validation<RR>> SelectMany<T, R, RR>
-      //   (this Task<Validation<T>> task    // Task<Validation<T>> 
-      //   , Func<T, Task<R>> bind           // -> (T -> Task<R>)
-      //   , Func<T, R, RR> project)
-      //   => task.Bind(vt => vt.Traverse(t => bind(t).Map(r => project(t, r))));
-
-      //public static Task<Validation<U>> SelectMany<T, U>
-      //   (this Task<Validation<T>> self
-      //   , Func<T, Task<U>> bind)
-      //   => self.Bind(vt => vt.Traverse(t => bind(t)));
-
-      //public static Task<Validation<V>> SelectMany<T, U, V>
-      //   (this Task<T> self
-      //   , Func<T, Validation<U>> bind
-      //   , Func<T, U, V> project)
-      //   => self.Map(t => bind(t).Map(u => project(t, u)));
-
-
-      //public static Task<Validation<V>> SelectMany<T, U, V>
-      //   (this Task<Validation<T>> self
-      //   , Func<T, U> bind
-      //   , Func<T, U, V> project)
-      //   => self.Map(x => x.SelectMany(bind, project));
-
-      //internal static Validation<V> SelectMany<T, U, V>
-      //   (this Validation<T> self
-      //   , Func<T, U> bind
-      //   , Func<T, U, V> project)
-      //   => self.Map(t => project(t, bind(t)));
+      public static TaskValidation<RR> SelectMany<T, R, RR>
+         (this TaskValidation<T> tv       // Task<Validation<T>> 
+         , Func<T, Task<Validation<R>>> bind  // -> (T -> Task<Validation<R>>)
+         , Func<T, R, RR> project)
+         => tv
+            .Value
+            .Map(vt => vt.TraverseBind(t => bind(t).Map(vr => vr.Map(r => project(t, r)))))
+            .Unwrap()
+            .Stack();
    }
 }
