d9b87ecbee72afcbc72cf4265c8ab7d02e64f0f5
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-26 20:17:24 +0200

    Task: Apply & Map with up to 9 arguments - Map implementated because of test

--- LaYumba.Functional.Tests/Task.cs
+++ LaYumba.Functional.Tests/Task.cs
@@ -54,4 +54,136 @@ namespace LaYumba.Functional.Tests
          await Assert.ThrowsAsync<Exception>(async () => await task);
       }
    }
+
+   public class Task_Apply_Test
+   {
+      async Task<string> Fail(string message = null) => await Task.FromException<string>(new Exception(message)); 
+      async Task<T> Succeed<T>(T t) => await Task.FromResult(t);
+      
+      Func<int, int, int> add = (a, b) => a + b;
+      Func<int, int, int> multiply = (i, j) => i * j;
+
+      private readonly Func<int, int, int, int> add3Integers = 
+         (a, b, c) => a + b + c;
+      private readonly Func<int, int, int, int, int> add4Integers = 
+         (a, b, c, d) => a + b + c + d;
+      private readonly Func<int, int, int, int, int, int> add5Integers = 
+         (a, b, c, d, e) => a + b + c + d + e;
+      private readonly Func<int, int, int, int, int, int, int> add6Integers = 
+         (a, b, c, d, e, f) => a + b + c + d + e + f;
+      private readonly Func<int, int, int, int, int, int, int, int> add7Integers = 
+         (a, b, c, d, e, f, g) => a + b + c + d + e + f + g;
+      private readonly Func<int, int, int, int, int, int, int, int, int> add8Integers = 
+         (a, b, c, d, e, f, g, h) => a + b + c + d + e + f + g + h;
+      private readonly Func<int, int, int, int, int, int, int, int, int, int> add9Integers = 
+         (a, b, c, d, e, f, g, h, i) => a + b + c + d + e + f + g + h + i;
+
+      [Fact]
+      public async void MapAndApplyArg_ReturnsExpectedContent()
+      {
+         var result = await Succeed(3)
+            .Map(add)
+            .Apply(Succeed(4));
+
+         Assert.Equal(7, result);
+      }
+
+      [Fact]
+      public async void MapAndApplyArg_to_function_requiring_3_arguments()
+      {
+         var result = await Succeed(1)
+            .Map(add3Integers)
+            .Apply(Succeed(2))
+            .Apply(Succeed(3));
+
+         Assert.Equal(6, result);
+      }
+
+      [Fact]
+      public async void MapAndApplyArg_to_function_requiring_4_arguments()
+      {
+         var result = await Succeed(1)
+            .Map(add4Integers)
+            .Apply(Succeed(2))
+            .Apply(Succeed(3))
+            .Apply(Succeed(4));
+
+         Assert.Equal(10, result);
+      }
+
+      [Fact]
+      public async void MapAndApplyArg_to_function_requiring_5_arguments()
+      {
+         var result = await Succeed(1)
+            .Map(add5Integers)
+            .Apply(Succeed(2))
+            .Apply(Succeed(3))
+            .Apply(Succeed(4))
+            .Apply(Succeed(5));
+
+         Assert.Equal(15, result);
+      }
+
+      [Fact]
+      public async void MapAndApplyArg_to_function_requiring_6_arguments()
+      {
+         var result = await Succeed(1)
+            .Map(add6Integers)
+            .Apply(Succeed(2))
+            .Apply(Succeed(3))
+            .Apply(Succeed(4))
+            .Apply(Succeed(5))
+            .Apply(Succeed(6));
+
+         Assert.Equal(21, result);
+      }
+
+      [Fact]
+      public async void MapAndApplyArg_to_function_requiring_7_arguments()
+      {
+         var result = await Succeed(1)
+            .Map(add7Integers)
+            .Apply(Succeed(2))
+            .Apply(Succeed(3))
+            .Apply(Succeed(4))
+            .Apply(Succeed(5))
+            .Apply(Succeed(6))
+            .Apply(Succeed(7));
+
+         Assert.Equal(28, result);
+      }
+
+      [Fact]
+      public async void MapAndApplyArg_to_function_requiring_8_arguments()
+      {
+         var result = await Succeed(1)
+            .Map(add8Integers)
+            .Apply(Succeed(2))
+            .Apply(Succeed(3))
+            .Apply(Succeed(4))
+            .Apply(Succeed(5))
+            .Apply(Succeed(6))
+            .Apply(Succeed(7))
+            .Apply(Succeed(8));
+
+         Assert.Equal(36, result);
+      }
+
+      [Fact]
+      public async void MapAndApplyArg_to_function_requiring_9_arguments()
+      {
+         var result = await Succeed(1)
+            .Map(add9Integers)
+            .Apply(Succeed(2))
+            .Apply(Succeed(3))
+            .Apply(Succeed(4))
+            .Apply(Succeed(5))
+            .Apply(Succeed(6))
+            .Apply(Succeed(7))
+            .Apply(Succeed(8))
+            .Apply(Succeed(9));
+
+         Assert.Equal(45, result);
+      }
+   }
 }
