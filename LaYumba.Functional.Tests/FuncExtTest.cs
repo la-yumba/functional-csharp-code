4f9b0e643b176c3c45fa74d0a81589faf83e9e2f
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-26 21:02:38 +0200

    Func: Apply with up to 9 arguments

--- LaYumba.Functional.Tests/FuncExtTest.cs
+++ LaYumba.Functional.Tests/FuncExtTest.cs
@@ -32,4 +32,131 @@ namespace LaYumba.Functional.Tests
             .Map(i => a(i) == b(i))
             .All(eq => eq));
    }
+
+   public class Func_Apply_Test
+   {
+      Func<int, int, int> add = (a, b) => a + b;
+
+      private readonly Func<int, int, int, int> add3Integers = 
+         (a, b, c) => a + b + c;
+      private readonly Func<int, int, int, int, int> add4Integers = 
+         (a, b, c, d) => a + b + c + d;
+      private readonly Func<int, int, int, int, int, int> add5Integers = 
+         (a, b, c, d, e) => a + b + c + d + e;
+      private readonly Func<int, int, int, int, int, int, int> add6Integers = 
+         (a, b, c, d, e, f) => a + b + c + d + e + f;
+      private readonly Func<int, int, int, int, int, int, int, int> add7Integers = 
+         (a, b, c, d, e, f, g) => a + b + c + d + e + f + g;
+      private readonly Func<int, int, int, int, int, int, int, int, int> add8Integers = 
+         (a, b, c, d, e, f, g, h) => a + b + c + d + e + f + g + h;
+      private readonly Func<int, int, int, int, int, int, int, int, int, int> add9Integers = 
+         (a, b, c, d, e, f, g, h, i) => a + b + c + d + e + f + g + h + i;
+
+      [Fact]
+      public void ApplyArgs()
+      {
+         var add1 = add.Apply(1);
+         var actual = add1(1);
+
+         Assert.Equal(2, actual);
+      }
+
+      [Fact]
+      public void ApplyArgs_to_function_requiring_3_args()
+      {
+         var f = add3Integers.Apply(1)
+            .Apply(2);
+
+         var result = f(3);
+         
+         Assert.Equal(6, result);
+      }
+
+      [Fact]
+      public void ApplyArgs_to_function_requiring_4_args()
+      {
+         var f = add4Integers.Apply(1)
+            .Apply(2)
+            .Apply(3);
+
+         var result = f(4);
+         
+         Assert.Equal(10, result);
+      }
+
+      [Fact]
+      public void ApplyArgs_to_function_requiring_5_args()
+      {
+         var f = add5Integers.Apply(1)
+            .Apply(2)
+            .Apply(3)
+            .Apply(4);
+
+         var result = f(5);
+         
+         Assert.Equal(15, result);
+      }
+
+      [Fact]
+      public void ApplyArgs_to_function_requiring_6_args()
+      {
+         var f = add6Integers.Apply(1)
+            .Apply(2)
+            .Apply(3)
+            .Apply(4)
+            .Apply(5);
+
+         var result = f(6);
+         
+         Assert.Equal(21, result);
+      }
+
+      [Fact]
+      public void ApplyArgs_to_function_requiring_7_args()
+      {
+         var f = add7Integers.Apply(1)
+            .Apply(2)
+            .Apply(3)
+            .Apply(4)
+            .Apply(5)
+            .Apply(6);
+
+         var result = f(7);
+         
+         Assert.Equal(28, result);
+      }
+
+      [Fact]
+      public void ApplyArgs_to_function_requiring_8_args()
+      {
+         var f = add8Integers.Apply(1)
+            .Apply(2)
+            .Apply(3)
+            .Apply(4)
+            .Apply(5)
+            .Apply(6)
+            .Apply(7);
+
+         var result = f(8);
+         
+         Assert.Equal(36, result);
+      }
+
+      [Fact]
+      public void ApplyArgs_to_function_requiring_9_args()
+      {
+         var f = add9Integers.Apply(1)
+            .Apply(2)
+            .Apply(3)
+            .Apply(4)
+            .Apply(5)
+            .Apply(6)
+            .Apply(7)
+            .Apply(8);
+
+         var result = f(9);
+         
+         Assert.Equal(45, result);
+      }
+   }
 }
--- LaYumba.Functional/FuncExt.cs
+++ LaYumba.Functional/FuncExt.cs
@@ -21,6 +21,24 @@ namespace LaYumba.Functional
       public static Func<T2, T3, R> Apply<T1, T2, T3, R>(this Func<T1, T2, T3, R> func, T1 t1)
           => (t2, t3) => func(t1, t2, t3);
 
+      public static Func<T2, T3, T4, R> Apply<T1, T2, T3, T4, R>(this Func<T1, T2, T3, T4, R> func, T1 t1)
+          => (t2, t3, t4) => func(t1, t2, t3, t4);
+
+      public static Func<T2, T3, T4, T5, R> Apply<T1, T2, T3, T4, T5, R>(this Func<T1, T2, T3, T4, T5, R> func, T1 t1)
+          => (t2, t3, t4, t5) => func(t1, t2, t3, t4, t5);
+
+      public static Func<T2, T3, T4, T5, T6, R> Apply<T1, T2, T3, T4, T5, T6, R>(this Func<T1, T2, T3, T4, T5, T6, R> func, T1 t1)
+          => (t2, t3, t4, t5, t6) => func(t1, t2, t3, t4, t5, t6);
+
+      public static Func<T2, T3, T4, T5, T6, T7, R> Apply<T1, T2, T3, T4, T5, T6, T7, R>(this Func<T1, T2, T3, T4, T5, T6, T7, R> func, T1 t1)
+          => (t2, t3, t4, t5, t6, t7) => func(t1, t2, t3, t4, t5, t6, t7);
+
+      public static Func<T2, T3, T4, T5, T6, T7, T8, R> Apply<T1, T2, T3, T4, T5, T6, T7, T8, R>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, R> func, T1 t1)
+          => (t2, t3, t4, t5, t6, t7, t8) => func(t1, t2, t3, t4, t5, t6, t7, t8);
+
+      public static Func<T2, T3, T4, T5, T6, T7, T8, T9, R> Apply<T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, R> func, T1 t1)
+          => (t2, t3, t4, t5, t6, t7, t8, t9) => func(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+
       public static Func<I1, I2, R> Map<I1, I2, T, R>(this Func<I1, I2, T> @this, Func<T, R> func)
          => (i1, i2) => func(@this(i1, i2));
    }
