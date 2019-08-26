3d4cc0b8f33fc033febd802e2fd40e910181b846
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-26 19:00:29 +0200

    Validation: Apply with up to 9 arguments

--- LaYumba.Functional.Tests/Valid.cs
+++ LaYumba.Functional.Tests/Valid.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 using Xunit;
 using static LaYumba.Functional.F;
@@ -11,6 +11,21 @@ namespace LaYumba.Functional.Tests
 
       Func<int, int, int> add = (a, b) => a + b;
       Func<int, int, int> Multiply = (i, j) => i * j;
+
+      private readonly Func<int, int, int, int> add3Integers = 
+         (a, b, c) => a + b + c;
+      private readonly Func<int, int, int, int, int> add4Integers = 
+         (a, b, c, d) => a + b + c + d;
+      private readonly Func<int, int, int, int, int, int> add5Integers = 
+         (a, b, c, d, e) => a + b + c + d + e;
+      private readonly Func<int, int, int, int, int, int, int> add6Integers = 
+         (a, b, c, d, e, f) => a + b + c + d + e + f;
+      private readonly Func<int, int, int, int, int, int, int, int> add7Integers = 
+         (a, b, c, d, e, f, g) => a + b + c + d + e + f + g;
+      private readonly Func<int, int, int, int, int, int, int, int, int> add8Integers = 
+         (a, b, c, d, e, f, g, h) => a + b + c + d + e + f + g + h;
+      private readonly Func<int, int, int, int, int, int, int, int, int, int> add9Integers = 
+         (a, b, c, d, e, f, g, h, i) => a + b + c + d + e + f + g + h + i;
 
       Func<string, Validation<int>> parseInt => 
          s => Int.Parse(s).Match(
@@ -88,6 +103,104 @@ namespace LaYumba.Functional.Tests
          Assert.Equal(Valid(7), opt);
       }
 
+      [Fact]
+      public void ApplySomeArgs_to_function_requiring_3_args()
+      {
+         var opt = Valid(add3Integers)
+            .Apply(Valid(1))
+            .Apply(Valid(2))
+            .Apply(Valid(3));
+
+         Assert.Equal(Valid(6), opt);
+      }
+
+      [Fact]
+      public void ApplySomeArgs_to_function_requiring_4_args()
+      {
+         var opt = Valid(add4Integers)
+            .Apply(Valid(1))
+            .Apply(Valid(2))
+            .Apply(Valid(3))
+            .Apply(Valid(4));
+
+         Assert.Equal(Valid(10), opt);
+      }
+
+      [Fact]
+      public void ApplySomeArgs_to_function_requiring_5_args()
+      {
+         var opt = Valid(add5Integers)
+            .Apply(Valid(1))
+            .Apply(Valid(2))
+            .Apply(Valid(3))
+            .Apply(Valid(4))
+            .Apply(Valid(5));
+
+         Assert.Equal(Valid(15), opt);
+      }
+
+      [Fact]
+      public void ApplySomeArgs_to_function_requiring_6_args()
+      {
+         var opt = Valid(add6Integers)
+            .Apply(Valid(1))
+            .Apply(Valid(2))
+            .Apply(Valid(3))
+            .Apply(Valid(4))
+            .Apply(Valid(5))
+            .Apply(Valid(6));
+
+         Assert.Equal(Valid(21), opt);
+      }
+
+      [Fact]
+      public void ApplySomeArgs_to_function_requiring_7_args()
+      {
+         var opt = Valid(add7Integers)
+            .Apply(Valid(1))
+            .Apply(Valid(2))
+            .Apply(Valid(3))
+            .Apply(Valid(4))
+            .Apply(Valid(5))
+            .Apply(Valid(6))
+            .Apply(Valid(7));
+
+         Assert.Equal(Valid(28), opt);
+      }
+
+      [Fact]
+      public void ApplySomeArgs_to_function_requiring_8_args()
+      {
+         var opt = Valid(add8Integers)
+            .Apply(Valid(1))
+            .Apply(Valid(2))
+            .Apply(Valid(3))
+            .Apply(Valid(4))
+            .Apply(Valid(5))
+            .Apply(Valid(6))
+            .Apply(Valid(7))
+            .Apply(Valid(8));
+
+         Assert.Equal(Valid(36), opt);
+      }
+
+      [Fact]
+      public void ApplySomeArgs_to_function_requiring_9_args()
+      {
+         var opt = Valid(add9Integers)
+            .Apply(Valid(1))
+            .Apply(Valid(2))
+            .Apply(Valid(3))
+            .Apply(Valid(4))
+            .Apply(Valid(5))
+            .Apply(Valid(6))
+            .Apply(Valid(7))
+            .Apply(Valid(8))
+            .Apply(Valid(9));
+
+         Assert.Equal(Valid(45), opt);
+      }
+      
       [Fact]
       public void ApplyNoneArgs()
       {
--- LaYumba.Functional/F.cs
+++ LaYumba.Functional/F.cs
@@ -21,6 +21,24 @@ namespace LaYumba.Functional
       public static Func<T1, Func<T2, T3, R>> CurryFirst<T1, T2, T3, R>
          (this Func<T1, T2, T3, R> @this) => t1 => (t2, t3) => @this(t1, t2, t3);
 
+      public static Func<T1, Func<T2, T3, T4, R>> CurryFirst<T1, T2, T3, T4, R>
+         (this Func<T1, T2, T3, T4, R> @this) => t1 => (t2, t3, t4) => @this(t1, t2, t3, t4);
+
+      public static Func<T1, Func<T2, T3, T4, T5, R>> CurryFirst<T1, T2, T3, T4, T5, R>
+         (this Func<T1, T2, T3, T4, T5, R> @this) => t1 => (t2, t3, t4, t5) => @this(t1, t2, t3, t4, t5);
+
+      public static Func<T1, Func<T2, T3, T4, T5, T6, R>> CurryFirst<T1, T2, T3, T4, T5, T6, R>
+         (this Func<T1, T2, T3, T4, T5, T6, R> @this) => t1 => (t2, t3, t4, t5, t6) => @this(t1, t2, t3, t4, t5, t6);
+
+      public static Func<T1, Func<T2, T3, T4, T5, T6, T7, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, R>
+         (this Func<T1, T2, T3, T4, T5, T6, T7, R> @this) => t1 => (t2, t3, t4, t5, t6, t7) => @this(t1, t2, t3, t4, t5, t6, t7);
+
+      public static Func<T1, Func<T2, T3, T4, T5, T6, T7, T8, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, T8, R>
+         (this Func<T1, T2, T3, T4, T5, T6, T7, T8, R> @this) => t1 => (t2, t3, t4, t5, t6, t7, t8) => @this(t1, t2, t3, t4, t5, t6, t7, t8);
+
+      public static Func<T1, Func<T2, T3, T4, T5, T6, T7, T8, T9, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, T8, T9, R>
+         (this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, R> @this) => t1 => (t2, t3, t4, t5, t6, t7, t8, t9) => @this(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+
       public static Func<T, T> Tap<T>(Action<T> act) 
          => x => { act(x); return x; };
       
